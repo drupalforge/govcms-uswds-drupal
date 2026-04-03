04542e444e051d0016927416931fd7a69504ff17
Darren Oh <9472-darrenoh@users.noreply.drupalcode.org>
2026-04-03 16:41:37 -0400

    Update PHP version to 8.5 and enhance DevPanel configurations

--- .devpanel/composer_setup.sh
+++ .devpanel/composer_setup.sh
@@ -6,12 +6,12 @@
 #set -eu -o pipefail
 cd $APP_ROOT
 
-# Create required composer.json and composer.lock files
+# Create required composer.json and composer.lock files.
 composer create-project --no-install ${PROJECT:=emanaton/govcms-uswds-drupal} --stability=dev
-cp -r "${PROJECT#*/}"/* ./
-rm -rf "${PROJECT#*/}" patches.lock.json
+cp -r ${PROJECT#*/}/* ./
+rm -rf ${PROJECT#*/}
 
-# Programmatically fix Composer 2.2 allow-plugins to avoid errors
+# Programmatically fix Composer 2.2 allow-plugins to avoid errors.
 composer config --no-plugins allow-plugins.cweagans/composer-patches true
 
 # Scaffold settings.php.
